0c20ab64ab30aa9468427b2493bc23f8eefd9a64
didigagu <didigagu@gmail.com>
2025-12-07 04:24:01 +0400

    Created 2 models Regions and Districts.

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Regions
+from .models import Regions, Districts
 
 class RegionsAdmin(admin.ModelAdmin):
-	list_display = ('name_ge', 'created', 'updated')
+	list_display = ('name_ge', 'created', 'updated') #Only applies to the Django Admin interface.
+	readonly_fields = ('created', 'updated') #Only applies to the Django Admin interface.
+	history_list_display = ["name_ge"] #Only applies to the Django Admin interface.
 
 admin.site.register(Regions, RegionsAdmin)
+
+class DistrictsAdmin(admin.ModelAdmin):
+	list_display = ('region', 'name_ge', 'created', 'updated')
+	readonly_fields = ('created', 'updated')
+	history_list_display = ["name_ge"]
+
+admin.site.register(Districts, DistrictsAdmin)
--- main_app/models.py
+++ main_app/models.py
@@ -13,11 +13,12 @@ def validate_not_whitespace(value):
 		)
 
 class Regions(models.Model):
-	created = models.DateTimeField(auto_now_add=True, verbose_name=_('Created'))
-	updated = models.DateTimeField(auto_now=True, verbose_name=_('Updated'))
+	created = models.DateTimeField(auto_now_add=True, verbose_name=_('შეიქმნა'))
+	updated = models.DateTimeField(auto_now=True, verbose_name=_('განახლდა'))
 	name_ge = models.CharField(
 		max_length=50,
 		verbose_name=_("რეგიონის დასახელება"),
+		unique=True,
 		blank=False,
 		null=False,
 		validators=[validate_not_whitespace]
@@ -27,6 +28,35 @@ class Regions(models.Model):
 	class Meta:
 		verbose_name = _('Region')
 		verbose_name_plural = _('Regions')
+		ordering = ['name_ge']
+
+	def __str__(self):
+		return self.name_ge
+
+
+class Districts(models.Model):
+	created = models.DateTimeField(auto_now_add=True, verbose_name=_('შეიქმნა'))
+	updated = models.DateTimeField(auto_now=True, verbose_name=_('განახლდა'))
+	name_ge = models.CharField(
+		max_length=50,
+		verbose_name=_("რაიონის/ქალაქის დასახელება"),
+		unique=True,
+		blank=False,
+		null=False,
+		validators=[validate_not_whitespace]
+	)
+	region = models.ForeignKey(
+		Regions,
+		on_delete=models.CASCADE,
+		related_name='districts',
+		verbose_name=_('რეგიონი'),
+		#default=1
+	)
+	history = HistoricalRecords()
+
+	class Meta:
+		verbose_name = _('District')
+		verbose_name_plural = _('Districts')
 		ordering = ['name_ge']
 
 	def __str__(self):
